bdf54bc00fcb71ef9e67692cddaa7b11695c7e04
BerkayGursoy21 <berkay.gursoy141@gmail.com>
2023-05-15 13:37:10 +0300

    Fight Reservation
    Mıd-Term Homework

---
+++ Berkay/prescriptions.cs
@@ -0,0 +1,41 @@
+﻿using Berkay.mains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Berkay
+{
+    internal class prescriptions
+    {
+        public string Customer;
+        public string Airplane;
+        public string Location;
+        public string Date;
+        public string bK;
+
+        public prescriptions(string Ad, string Soyad,string airplane,string location,string date,string bk)
+        {
+
+            Customer = Ad + " " + Soyad;
+            Airplane = airplane;
+            Location = location;    
+            Date = date;
+            bK = bk;
+
+        }
+        public string UcusDondur()
+        {
+            //uçuş bilgileri
+
+            Reservation rezervasyon = new Reservation();
+            
+            string Temp = "\n NEREDEN: " + bK + "\n NEREYE: " + Location + "\n Uçuş Tarihiniz: " + Date +  "\n ADINIZ SOYADINIZ: " + Customer + "\n Uçağınız: " + Airplane +  "\n Koltuk Numaranız: " + rezervasyon.PassangerNumber + "\n Bilet Tutarınız: " + rezervasyon.Price + "TL";            
+            return Temp;
+
+        }
+    }
+
+}
